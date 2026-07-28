0ced7d8daae9ae38c82c7498fad4076ff7663d34
Jinghao Hu <731409213@qq.com>
2026-07-29 03:19:17 +0800

    register assault direction symbol

--- packages/symbols/src/catalog.ts
+++ packages/symbols/src/catalog.ts
@@ -1,4 +1,5 @@
 import type { PlotDefinition } from "@plotlibre/core";
+import { assaultDirectionDefinition } from "./assault-direction.js";
 import { fineArrowDefinition } from "./fine-arrow.js";
 import { straightArrowDefinition } from "./straight-arrow.js";
 import { tailedFineArrowDefinition } from "./tailed-fine-arrow.js";
@@ -7,6 +8,7 @@ export const arrowSymbols: readonly PlotDefinition[] = [
   straightArrowDefinition,
   fineArrowDefinition,
   tailedFineArrowDefinition,
+  assaultDirectionDefinition,
 ];
 
 export const builtInSymbols: readonly PlotDefinition[] = [...arrowSymbols];
