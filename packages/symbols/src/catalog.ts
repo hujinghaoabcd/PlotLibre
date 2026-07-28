6f06e4228d183761c2bb5240cd3f38f0602c93f7
Jinghao Hu <731409213@qq.com>
2026-07-29 02:25:38 +0800

    register fine arrow symbol

--- packages/symbols/src/catalog.ts
+++ packages/symbols/src/catalog.ts
@@ -1,8 +1,10 @@
 import type { PlotDefinition } from "@plotlibre/core";
+import { fineArrowDefinition } from "./fine-arrow.js";
 import { straightArrowDefinition } from "./straight-arrow.js";
 
 export const arrowSymbols: readonly PlotDefinition[] = [
   straightArrowDefinition,
+  fineArrowDefinition,
 ];
 
 export const builtInSymbols: readonly PlotDefinition[] = [...arrowSymbols];
