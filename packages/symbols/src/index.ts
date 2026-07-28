7596cb469371d0434c66313bc767528991eaab17
Jinghao Hu <731409213@qq.com>
2026-07-29 04:15:27 +0800

    export curved arrow symbol

--- packages/symbols/src/index.ts
+++ packages/symbols/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./assault-direction.js";
 export * from "./catalog.js";
+export * from "./curved-arrow.js";
 export * from "./fine-arrow.js";
 export * from "./straight-arrow.js";
 export * from "./style.js";
