061374818f332942b44e4796769ee8c1b8c8aa4e
Jinghao Hu <731409213@qq.com>
2026-07-29 10:56:39 +0800

    export tailed attack arrow symbol

--- packages/symbols/src/index.ts
+++ packages/symbols/src/index.ts
@@ -5,4 +5,5 @@ export * from "./curved-arrow.js";
 export * from "./fine-arrow.js";
 export * from "./straight-arrow.js";
 export * from "./style.js";
+export * from "./tailed-attack-arrow.js";
 export * from "./tailed-fine-arrow.js";
