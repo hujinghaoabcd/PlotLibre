197c8d732f706ff96e3aa2150668afefa697521b
Jinghao Hu <731409213@qq.com>
2026-08-05 02:17:06 +0800

    Export selection local transforms

--- packages/interaction/src/index.ts
+++ packages/interaction/src/index.ts
@@ -4,5 +4,6 @@ export * from "./multi-point-draw-session.js";
 export * from "./screen-region-selection.js";
 export * from "./screen-selection-region-session.js";
 export * from "./selection-controller.js";
+export * from "./selection-local-transform.js";
 export * from "./two-point-draw-session.js";
 export * from "./types.js";
