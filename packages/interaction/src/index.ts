915fc9a1e3fd08eefc64a1b96d0c0380cf187b20
Jinghao Hu <731409213@qq.com>
2026-08-04 21:31:01 +0800

    Export screen-region selection utilities

--- packages/interaction/src/index.ts
+++ packages/interaction/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./batch-edit-command.js";
 export * from "./local-translation.js";
 export * from "./multi-point-draw-session.js";
+export * from "./screen-region-selection.js";
 export * from "./selection-controller.js";
 export * from "./two-point-draw-session.js";
 export * from "./types.js";
