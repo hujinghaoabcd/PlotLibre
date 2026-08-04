0cf8e2c905b39ac9d5509818b1a2db4f997ef9a6
Jinghao Hu <731409213@qq.com>
2026-08-04 21:39:54 +0800

    Export selection region resolver

--- packages/maplibre/src/index.ts
+++ packages/maplibre/src/index.ts
@@ -2,5 +2,6 @@ export * from "./interaction.js";
 export * from "./plotlibre.js";
 export * from "./renderer.js";
 export * from "./selection-modifier-capture.js";
+export * from "./selection-region-resolver.js";
 export * from "./selection-translation.js";
 export * from "./types.js";
